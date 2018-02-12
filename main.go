f6bfc517bfc8fbc380fba5cc23b096f0d9d589eb
Alexander Sheiko <alexander.sheiko@mirs.com>
2018-02-12 13:38:36 +0200

    Rewrite checkout/pull Optional print of the last commit Minor fixes

--- main.go
+++ main.go
@@ -13,15 +13,17 @@ import (
 )
 
 var (
-	Repository   = kingpin.Arg("repository", "The repository to clone from.").Required().String()
-	Directory    = kingpin.Arg("directory", "The name of a new Directory to clone into.").String()
-	Recursive    = kingpin.Flag("recursive", "After the clone is created, initialize all submodules within, using their default settings.").Short('r').Bool()
-	Pull         = kingpin.Flag("pull", "Incorporates changes from a remote repository into the current branch (if already cloned).").Short('p').Bool()
-	RemoteName   = kingpin.Flag("origin", "Instead of using the remote name origin to keep track of the upstream repository, use <name>.").Short('o').PlaceHolder("<name>").Default("origin").String()
-	Branch       = kingpin.Flag("branch", "Instead of pointing the newly created HEAD to the branch pointed to by the cloned repository’s HEAD, point to <name> branch instead.").Short('b').PlaceHolder("<name>").String()
+	Repository = kingpin.Arg("repository", "The repository to clone from.").Required().String()
+	Directory  = kingpin.Arg("directory", "The name of a new Directory to clone into.").String()
+	Recursive  = kingpin.Flag("recursive", "After the clone is created, initialize all submodules within, using their default settings.").Short('r').Bool()
+	Pull       = kingpin.Flag("pull", "Incorporates changes from a remote repository into the current branch (if already cloned).").Short('p').Bool()
+	RemoteName = kingpin.Flag("origin", "Instead of using the remote name origin to keep track of the upstream repository, use <name>.").Short('o').PlaceHolder("<name>").Default("origin").String()
+	Branch     = kingpin.Flag("branch", "Instead of pointing the newly created HEAD to the branch pointed to by the cloned repository’s HEAD, point to <name> branch instead. "+
+		"If the repository already cloned, it will simply switch the branch, and local changes will be discarded.").Short('b').PlaceHolder("<name>").String()
 	SingleBranch = kingpin.Flag("single-branch", "Clone only the history leading to the tip of a single branch, either specified by the --branch option or the primary branch remote’s HEAD points at.").Bool()
-	Depth        = kingpin.Flag("depth", "Create a shallow clone with a history truncated to the specified number of commits.").Short('d').Default("0").Int()
+	Depth        = kingpin.Flag("depth", "Create a shallow clone with a history truncated to the specified number of commits.").Short('d').PlaceHolder("<depth>").Int()
 	Tags         = kingpin.Flag("tags", "Tag mode (all|no|following)").Default("all").Enum("all", "no", "following")
+	LastCommit   = kingpin.Flag("last", "Print the latest commit.").Short('l').Bool()
 )
 
 func CheckIfError(err error) {
@@ -34,7 +36,6 @@ func CheckIfError(err error) {
 }
 
 func main() {
-
 	kingpin.Parse()
 
 	var Destination string
@@ -67,32 +68,77 @@ func main() {
 
 	r, err := git.PlainClone(Destination, false, &CloneOptions)
 
-	if err == git.ErrRepositoryAlreadyExists && *Pull {
+	if err == git.ErrRepositoryAlreadyExists {
 		color.Yellow("Repository already exists!")
+
 		r, err := git.PlainOpen(Destination)
 		CheckIfError(err)
 
 		w, err := r.Worktree()
 		CheckIfError(err)
 
-		color.Cyan("Try pull")
-		err = w.Pull(&git.PullOptions{RemoteName: "origin"})
-		if err == git.NoErrAlreadyUpToDate {
-			color.Green(err.Error())
-		} else {
+		ref, err := r.Head()
+		CheckIfError(err)
+
+		if len(*Branch) > 0 && !strings.HasSuffix(ref.Name().String(), *Branch) {
+			color.Cyan("Checkout remote branch %s", *Branch)
+			branchRef := plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", *Branch))
+			err = w.Checkout(&git.CheckoutOptions{
+				Branch: branchRef,
+				Force:  true,
+			})
+			if err == plumbing.ErrReferenceNotFound {
+				remoteRef, err := r.Reference(plumbing.ReferenceName(fmt.Sprintf("refs/remotes/%s/%s", *RemoteName, *Branch)), true)
+				CheckIfError(err)
+				err = w.Checkout(&git.CheckoutOptions{
+					Branch: branchRef,
+					Hash:   remoteRef.Hash(),
+					Create: true,
+					Force:  true,
+				})
+			} else {
+				CheckIfError(err)
+			}
+			ref, err = r.Head()
 			CheckIfError(err)
+		}
+		if *Pull {
+			color.Cyan("Pull %s", ref.Name())
+			PullOptions := git.PullOptions{
+				RemoteName:    *RemoteName,
+				ReferenceName: ref.Name(),
+				Depth:         *Depth,
+				SingleBranch:  *SingleBranch,
+				Progress:      os.Stdout,
+			}
+			if *Recursive {
+				PullOptions.RecurseSubmodules = git.DefaultSubmoduleRecursionDepth
+			}
+			err = w.Pull(&PullOptions)
+			if err == git.NoErrAlreadyUpToDate {
+				color.Green(err.Error())
+			} else {
+				CheckIfError(err)
+			}
 		}
 	} else if err != nil {
 		CheckIfError(err)
 	}
 
-	fmt.Println()
+	if *LastCommit {
+		fmt.Println()
 
-	ref, err := r.Head()
-	CheckIfError(err)
+		if r == nil {
+			r, err = git.PlainOpen(Destination)
+			CheckIfError(err)
+		}
 
-	commit, err := r.CommitObject(ref.Hash())
-	CheckIfError(err)
-	fmt.Println("Show last commit:")
-	fmt.Println(commit)
+		ref, err := r.Head()
+		CheckIfError(err)
+
+		commit, err := r.CommitObject(ref.Hash())
+		CheckIfError(err)
+		fmt.Print("Show last ")
+		fmt.Println(commit)
+	}
 }
